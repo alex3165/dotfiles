aff7fb1fe25d450e2312c230d599573fc5ca79d4
Alexandre Rieux <alexr.3165@gmail.com>
2017-04-26 22:37:54 +0100

    Add vscode plugins

---
+++ install_vscode_plugins.sh
@@ -0,0 +1,9 @@
+code --install-extension EditorConfig.EditorConfig
+code --install-extension anseki.vscode-color
+code --install-extension eg2.tslint
+code --install-extension mohsen1.prettify-json
+code --install-extension ms-vscode.sublime-keybindings
+code --install-extension pmneo.tsimporter
+code --install-extension ryu1kn.annotator
+code --install-extension sbrink.elm
+code --install-extension waderyan.gitblame
